d522a3f66b30a316763b47fb958a8f937eb1cba6
katerina751 <katerina751@bk.ru>
2023-02-07 22:09:33 +0500

    git commit -m 'Added file index.js'

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Домашнее задание 4
+
 // задание 1
 
 let i = 1;
@@ -60,3 +62,93 @@ for (let day = firstFriday; day <= 31; day += 7) {
     console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчёт.`)
 
 };
+
+// Домашняя работа 5
+
+// задание 1
+
+function less(a, b) {
+    if (a <= b) {
+        return (a);
+    } else {
+        return (b)
+    }
+}
+
+less(12, -17);
+
+// задание 2
+
+function parity(n2) {
+    if (n2 % 2 == 0) {
+        return ('Число четное');
+    } else {
+        return ('Число нечетное')
+    }
+}
+parity(122);
+
+// задание 3
+
+function square(z) {
+    console.log(z ** 2)
+}
+
+square(7);
+
+function square1(z) {
+    return z ** 2;
+}
+
+square1(5);
+
+// задание 4
+
+let userAnswer = prompt("Сколько Вам лет?");
+
+if (userAnswer < 0) {
+    function printMessage() {
+        alert("Вы ввели неправильное значение");
+    };
+
+} else if (userAnswer >= 0 && userAnswer < 12) {
+    function printMessage() {
+        alert("Привет, друг!");
+    };
+} else if (userAnswer >= 13) {
+    function printMessage() {
+        alert("Добро пожаловать!");
+    }
+};
+
+printMessage();
+
+// задание 5
+
+let anuNumeric = (p, o) => {
+    if (isNaN(p, o)) return `Одно или оба значения не являются числом`;
+    else return (p * o);
+}
+
+anuNumeric(2, 7);
+
+// задание 6
+
+let numericUser = (a) => {
+    if (isNaN(a)) return `Переданный параметр не является числом`;
+    else return `${a} в кубе равняется ${a ** 3}`;
+}
+
+numericUser(Number(prompt('Введите число')));
+
+// задание 7
+
+let monthNumber = (xx) => {
+    if (xx === Number(12 || 1 || 2)) return `Зима`;
+    else if (xx === Number(3 || 4 || 5)) return `Весна`;
+    else if (xx === Number(6 || 7 || 8)) return `Лето`;
+    else if (xx === Number(9 || 10 || 11)) return `Осень`;
+    else return `Переданный параметр не является номером месяца`;
+}
+
+monthNumber(Number(prompt('Введите номер месяца')));
